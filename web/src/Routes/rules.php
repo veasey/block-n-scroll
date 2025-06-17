ce6286a64d4fbba081c2578f6c73d5e84a20a719
Clint Veasey <clintvee@gmail.com>
2025-06-17 11:24:36 +0100

    add skill list and individual skill template

--- web/src/Routes/rules.php
+++ web/src/Routes/rules.php
@@ -1,5 +1,6 @@
 <?php
 use App\Models\Base\BaseTeam;
+use App\Models\Base\Skill;
 use Slim\Views\Twig;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
@@ -42,4 +43,32 @@ $app->get('/rules/teams/{team_id}[.{format}]', function (Request $request, Respo
     }
 
     return Twig::fromRequest($request)->render($response, 'rules/team.twig', $data);
+});
+
+$app->get('/rules/skills[.{format}]', function (Request $request, Response $response, array $args) use ($app) {
+    $format = $args['format'] ?? 'html';
+    $skills = Skill::all();
+
+    if ($format === 'json') {
+        $response->getBody()->write($skills->toJson());
+        return $response->withHeader('Content-Type', 'application/json');
+    }
+
+    return Twig::fromRequest($request)->render($response, 'rules/skills.twig', ['skills' => $skills]);
+});
+
+$app->get('/rules/skill/{skill_id}[.{format}]', function (Request $request, Response $response, array $args) use ($app) {
+    $format = $args['format'] ?? 'html';
+
+    $skill = Skill::find($args['skill_id']);
+    if (!$skill) {
+        return $response->withStatus(404)->write('Skill not found');
+    }
+
+    if ($format === 'json') {
+        $response->getBody()->write(json_encode($skill));
+        return $response->withHeader('Content-Type', 'application/json');
+    }
+
+    return Twig::fromRequest($request)->render($response, 'rules/skill.twig', ['skill' => $skill]);
 });
