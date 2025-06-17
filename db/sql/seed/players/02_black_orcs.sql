a0d8ce6760fc646d3f047ca4e9dfeac7fdb4b590
Clint Veasey <clintvee@gmail.com>
2025-06-17 11:18:09 +0100

    fixed some special characters in seed text

--- db/sql/seed/players/02_black_orcs.sql
+++ db/sql/seed/players/02_black_orcs.sql
@@ -1,9 +1,9 @@
 INSERT INTO base_team_player
 (id, name, description, category, base_team_id, category_regional_special_rule_id, ma, st, ag, pa, av, primary_skills, secondary_skills, cost, max_count)
 VALUES
-(4, 'Black Orc', 'Tough and strong standard player', 'standard', 1, 1, 5, 5, 2, 2, 9, 'Strength', 'General', 140, 6),
-(5, 'Black Orc Thrower', 'Specialized thrower with strength', 'special', 1, 1, 5, 5, 2, 4, 9, 'Passing', 'Strength', 170, 1),
-(6, 'Black Orc Blitzer', 'Blitz-focused Black Orc', 'special', 1, 1, 5, 6, 2, 2, 10, 'Strength', 'General', 180, 2);
+(4, 'Black Orc', 'Tough and strong standard player', 'standard', 2, 1, 5, 5, 2, 2, 9, 'Strength', 'General', 140, 6),
+(5, 'Black Orc Thrower', 'Specialized thrower with strength', 'special', 2, 1, 5, 5, 2, 4, 9, 'Passing', 'Strength', 170, 1),
+(6, 'Black Orc Blitzer', 'Blitz-focused Black Orc', 'special', 2, 1, 5, 6, 2, 2, 10, 'Strength', 'General', 180, 2);
 
 -- Assuming IDs assigned as 1, 2, 3 for Black Orc, Thrower, Blitzer respectively:
 
@@ -23,7 +23,7 @@ VALUES
 INSERT INTO base_team_player
 (id, name, description, category, base_team_id, category_regional_special_rule_id, ma, st, ag, pa, av, primary_skills, secondary_skills, cost, max_count)
 VALUES
-(7, 'Goblin', 'Nimble but fragile support players', 'standard', 1, 1, 6, 2, 3, 4, 7, 'Agility', 'General', 40, 12);
+(7, 'Goblin', 'Nimble but fragile support players', 'standard', 2, 1, 6, 2, 3, 4, 7, 'Agility', 'General', 40, 12);
 
 -- Assuming this is the 4th inserted position (ID = 4):
 INSERT INTO base_team_player_skill (base_team_player_id, skill_id) VALUES
