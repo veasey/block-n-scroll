af72c6bc1a3e6109564310e3bf0f5e862c0cd06a
Clint Veasey <clintvee@gmail.com>
2025-06-18 10:51:04 +0100

    add elven union

---
+++ db/sql/seed/players/07_elven_union.sql
@@ -0,0 +1,46 @@
+INSERT INTO base_team_player
+(id, name, description, category, base_team_id, ma, st, ag, pa, av, cost, max_count)
+VALUES
+(70, 'Linemen', 'Basic Elven Union player', 'standard', 7, 6, 3, 2, 4, 8, 60000, 12),
+(71, 'Thrower', 'Skilled passer with Pass skill', 'standard', 7, 6, 3, 2, 2, 8, 75000, 2),
+(72, 'Catcher', 'Fast, with Catch and Nerves of Steel', 'standard', 7, 8, 3, 2, 4, 8, 100000, 4),
+(73, 'Blitzer', 'Powerful, with Block and Side Step', 'standard', 7, 7, 3, 2, 3, 9, 115000, 2);
+
+-- Linemen
+INSERT INTO base_team_player_skill_category (base_team_player_id, skill_category_id, skill_category_type) VALUES
+(70, 3, 'primary'),  -- AG
+(70, 2, 'secondary'); -- S
+
+-- Thrower
+INSERT INTO base_team_player_skill_category (base_team_player_id, skill_category_id, skill_category_type) VALUES
+(71, 3, 'primary'),  -- AG
+(71, 1, 'primary'),  -- P
+(71, 2, 'secondary'); -- S
+
+-- Catcher
+INSERT INTO base_team_player_skill_category (base_team_player_id, skill_category_id, skill_category_type) VALUES
+(72, 3, 'primary'),  -- AG
+(72, 2, 'secondary'); -- S
+
+-- Blitzer
+INSERT INTO base_team_player_skill_category (base_team_player_id, skill_category_id, skill_category_type) VALUES
+(73, 3, 'primary'),  -- AG
+(73, 2, 'secondary'); -- S
+
+-- Thrower (Pass)
+INSERT INTO base_team_player_skill (base_team_player_id, skill_id) VALUES
+(71, 101);  -- Pass
+
+-- Catcher (Catch, Nerves of Steel)
+INSERT INTO base_team_player_skill (base_team_player_id, skill_id) VALUES
+(72, 102),  -- Catch
+(72, 103);  -- Nerves of Steel
+
+-- Blitzer (Block, Side Step)
+INSERT INTO base_team_player_skill (base_team_player_id, skill_id) VALUES
+(73, 1),  -- Block
+(73, 104); -- Side Step
+
+-- Add Team to Region
+INSERT INTO base_team_regional_rule (base_team_id, special_rule_id) VALUES
+(7, 2); -- elven kingdom
