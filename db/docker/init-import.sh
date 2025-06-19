677d677965d0d8be80b14769fab1c2b510085cbb
Clint Veasey <clintvee@gmail.com>
2025-06-19 13:18:28 +0100

    silence warning, because was entering password in command line

--- db/docker/init-import.sh
+++ db/docker/init-import.sh
@@ -7,28 +7,36 @@ until mysqladmin ping -h localhost --silent; do
   sleep 1
 done
 
+echo "Creating .my.cnf"
+cat > ~/.my.cnf <<EOF
+[client]
+user=$MYSQL_USER
+password=$MYSQL_PASSWORD
+EOF
+chmod 600 ~/.my.cnf
+
 echo "Importing schema files in alphabetical order..."
 for file in /docker-entrypoint-initdb.d/sql/schema/*.sql; do
     echo "Importing $(basename "$file")..."
-    mysql -u "$MYSQL_ROOT_USER" -p"$MYSQL_ROOT_PASSWORD" "$MYSQL_DATABASE" < "$file"
+    mysql "$MYSQL_DATABASE" < "$file"
 done
 
 echo "Importing seeds in alphabetical order..."
 for sql_file in /docker-entrypoint-initdb.d/sql/seed/*.sql; do
   echo "Importing $sql_file..."
-  mysql -u "$MYSQL_ROOT_USER" -p"$MYSQL_ROOT_PASSWORD" "$MYSQL_DATABASE" < "$sql_file"
+  mysql "$MYSQL_DATABASE" < "$sql_file"
 done
 
 echo "Importing default name seeds in alphabetical order..."
 for sql_file in /docker-entrypoint-initdb.d/sql/seed/default_names/*.sql; do
   echo "Importing $sql_file..."
-  mysql -u "$MYSQL_ROOT_USER" -p"$MYSQL_ROOT_PASSWORD" "$MYSQL_DATABASE" < "$sql_file"
+  mysql "$MYSQL_DATABASE" < "$sql_file"
 done
 
 echo "Importing default race positions seeds in alphabetical order..."
 for sql_file in /docker-entrypoint-initdb.d/sql/seed/players/*.sql; do
   echo "Importing $sql_file..."
-  mysql -u "$MYSQL_ROOT_USER" -p"$MYSQL_ROOT_PASSWORD" "$MYSQL_DATABASE" < "$sql_file"
+  mysql "$MYSQL_DATABASE" < "$sql_file"
 done
 
 echo "All imports completed."
