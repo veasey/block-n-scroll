13f4c1a522f1ce8305588a1dc11439bdd8656e07
Clint Veasey <clintvee@gmail.com>
2025-08-05 15:24:16 +0100

    add latest matches to home page, and tweak view match template

--- web/src/Routes/web.php
+++ web/src/Routes/web.php
@@ -3,6 +3,8 @@ use Slim\Views\Twig;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
+use App\Models\MatchGame as Matches;
+
 $app->get('/', function (Request $request, Response $response) use ($app) {
-    return Twig::fromRequest($request)->render($response, 'home.twig');
+    return Twig::fromRequest($request)->render($response, 'home.twig', ['matches' => Matches::orderBy('updated_at', 'desc')->take(9)->get()]);
 });
