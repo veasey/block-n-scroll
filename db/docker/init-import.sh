54ade5396a8018aeb36ee397ded2c56351a8829b
Clint Veasey <clintvee@gmail.com>
2025-09-05 11:32:56 +0100

    change internal port mysql runs on, because windows has decided to block it now

--- db/docker/init-import.sh
+++ db/docker/init-import.sh
@@ -21,12 +21,6 @@ for file in /docker-entrypoint-initdb.d/sql/schema/*.sql; do
     mysql "$MYSQL_DATABASE" < "$file"
 done
 
-echo "Importing default name seeds in alphabetical order..."
-for sql_file in /docker-entrypoint-initdb.d/sql/seed/default_names/*.sql; do
-  echo "Importing $sql_file..."
-  mysql "$MYSQL_DATABASE" < "$sql_file"
-done
-
 echo "Importing seeds in alphabetical order..."
 for sql_file in /docker-entrypoint-initdb.d/sql/seed/*.sql; do
   echo "Importing $sql_file..."
@@ -35,6 +29,12 @@ done
 
 echo "Importing default race positions seeds in alphabetical order..."
 for sql_file in /docker-entrypoint-initdb.d/sql/seed/players/*/*.sql; do
+  echo "Importing $sql_file..."
+  mysql "$MYSQL_DATABASE" < "$sql_file"
+done
+
+echo "Importing default name seeds in alphabetical order..."
+for sql_file in /docker-entrypoint-initdb.d/sql/seed/default_names/*.sql; do
   echo "Importing $sql_file..."
   mysql "$MYSQL_DATABASE" < "$sql_file"
 done
